8759a5eb63d6f678c66f81d5c2c3179f4da49126
Dorsg <70431706+Dorsg@users.noreply.github.com>
2021-07-30 12:49:42 +0300

    basic UI completed

--- FacebookWinFormsApp/FcbForm.cs
+++ FacebookWinFormsApp/FcbForm.cs
@@ -8,19 +8,28 @@ using System.Text;
 using System.Windows.Forms;
 using FacebookWrapper.ObjectModel;
 using FacebookWrapper;
+using System.Runtime.InteropServices;
 
 namespace BasicFacebookFeatures
 {
      public partial class FcbForm : Form
      {
           User m_LoggedInUser;
+
+          private Button m_CurrentButton;
+          private Form m_ActiveForm;
           public FcbForm(User i_LoggedInUser)
           {
                InitializeComponent();
                m_LoggedInUser = i_LoggedInUser;
-               fetchUserInfo();
-               this.Text = $"Logged in as {m_LoggedInUser.Name}";
+               fetchUserInfo();         
+               
           }
+          [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+          private extern static void ReleaseCapture();
+          [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+          private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+
 
           private void FcbForm_Load(object sender, EventArgs e)
           {
@@ -29,26 +38,129 @@ namespace BasicFacebookFeatures
 
           private void fetchUserInfo()
           {
+               m_LabelTitle.Text = "Facebook";
+               //m_LabelTitle.Text = $"Facebook | Logged in as {m_LoggedInUser.Name}";
                pictureBoxProfile.Image = m_LoggedInUser.ImageLarge;
+               m_LabelName.Text = "Name: " + m_LoggedInUser.Name;
+               m_LabelBirthday.Text = "Birthday: " + m_LoggedInUser.Birthday;
+               m_LabelGender.Text = "Gender: " + m_LoggedInUser.Gender;
+               m_LabelEmail.Text = "Email: " + m_LoggedInUser.Email;
+               this.Text = string.Empty;
+               this.ControlBox = false;
+
                //if (m_LoggedInUser.Posts.Count > 0)
                //{
                //     textBoxStatus.Text = m_LoggedInUser.Posts[0].Message;
                //}
           }
 
-          private void label1_Click(object sender, EventArgs e)
-          {
 
+
+          private void ActivateButton(object btnSender)
+          {
+               if (btnSender != null)
+               {
+                    if (m_CurrentButton != (Button)btnSender)
+                    {
+                         disableButton();
+                         Color color = ColorTranslator.FromHtml("#166ADA");
+                         m_CurrentButton = (Button)btnSender;
+                         m_CurrentButton.BackColor = color;
+                         m_CurrentButton.ForeColor = Color.White;
+                         m_CurrentButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+
+
+                    }
+               }
           }
 
-          private void pictureBoxProfile_Click(object sender, EventArgs e)
+          private void disableButton()
           {
-
+               foreach (Control previousBtn in m_PanelMenu.Controls)
+               {
+                    if (previousBtn.GetType() == typeof(Button))
+                    {
+                         previousBtn.BackColor = Color.FromArgb(51, 51, 76);
+                         previousBtn.ForeColor = Color.Gainsboro;
+                         previousBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                    }
+               }
           }
 
-          private void label_Name_Click(object sender, EventArgs e)
+          private void OpenChildForm(Form childForm, object btnSender)
           {
+               if (m_ActiveForm != null)
+               {
+                    m_ActiveForm.Close();
+               }
 
+               ActivateButton(btnSender);
+               m_ActiveForm = childForm;
+               childForm.TopLevel = false;
+               childForm.FormBorderStyle = FormBorderStyle.None;
+               childForm.Dock = DockStyle.Fill;
+               m_PanelDesktop.Controls.Add(childForm);
+               m_PanelDesktop.Tag = childForm;
+               childForm.BringToFront();
+               childForm.Show();
+               m_LabelTitle.Text = childForm.Text;
+          }
+
+          private void m_ButtonStatus_Click(object sender, EventArgs e)
+          {
+               ///OpenChildForm(new Form1(), sender);
+          }
+
+          private void m_ButtonAlbums_Click(object sender, EventArgs e)
+          {
+               ActivateButton(sender);
+          }
+
+          private void m_buttonGroups_Click(object sender, EventArgs e)
+          {
+               ActivateButton(sender);
+          }
+
+          private void m_ButtonPages_Click(object sender, EventArgs e)
+          {
+               ActivateButton(sender);
+          }
+
+          private void m_ButtonEvents_Click(object sender, EventArgs e)
+          {
+               ActivateButton(sender);
+          }
+
+          private void m_ButtonMyFeature1_Click(object sender, EventArgs e)
+          {
+               ActivateButton(sender);
+          }
+
+          private void m_buttonMyFeature2_Click(object sender, EventArgs e)
+          {
+               ActivateButton(sender);
+          }
+
+          private void m_ButtonSettins_Click(object sender, EventArgs e)
+          {
+               ActivateButton(sender);
+          }
+
+          private void m_ButtonLogout_Click(object sender, EventArgs e)
+          {
+               ActivateButton(sender);
+          }
+
+          private void m_TtilePanel_MouseDown(object sender, MouseEventArgs e)
+          {
+               ReleaseCapture();
+               SendMessage(this.Handle, 0x112, 0xf012, 0);
+          }
+
+          private void m_PanelDesktop_MouseDown(object sender, MouseEventArgs e)
+          {
+               ReleaseCapture();
+               SendMessage(this.Handle, 0x112, 0xf012, 0);
           }
      }
 }
--- FacebookWinFormsApp/Properties/Resources.Designer.cs
+++ FacebookWinFormsApp/Properties/Resources.Designer.cs
@@ -59,5 +59,215 @@ namespace BasicFacebookFeatures.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _216308_pages_icon {
+            get {
+                object obj = ResourceManager.GetObject("216308_pages_icon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _2180486_calendar_events_fb_icon {
+            get {
+                object obj = ResourceManager.GetObject("2180486_calendar_events_fb_icon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _2239660_calendar_date_events_icon {
+            get {
+                object obj = ResourceManager.GetObject("2239660_calendar_date_events_icon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _2488797 {
+            get {
+                object obj = ResourceManager.GetObject("2488797", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _2561383_settings_icon {
+            get {
+                object obj = ResourceManager.GetObject("2561383_settings_icon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _309041_users_group_people_icon {
+            get {
+                object obj = ResourceManager.GetObject("309041_users_group_people_icon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _3209289_copy_files_pages_papers_print_icon {
+            get {
+                object obj = ResourceManager.GetObject("3209289_copy_files_pages_papers_print_icon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _3209289_copy_files_pages_papers_print_icon1 {
+            get {
+                object obj = ResourceManager.GetObject("3209289_copy_files_pages_papers_print_icon1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _3330248 {
+            get {
+                object obj = ResourceManager.GetObject("3330248", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _33302481 {
+            get {
+                object obj = ResourceManager.GetObject("33302481", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _33302482 {
+            get {
+                object obj = ResourceManager.GetObject("33302482", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _4115235_exit_logout_sign_out_icon {
+            get {
+                object obj = ResourceManager.GetObject("4115235_exit_logout_sign out_icon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _6590519_communication_email_letter_post_icon {
+            get {
+                object obj = ResourceManager.GetObject("6590519_communication_email_letter_post_icon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _751098_events_date_event_party_schedule_icon {
+            get {
+                object obj = ResourceManager.GetObject("751098_events_date_event_party_schedule_icon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _809486_albums_media_multimedia_photo_album_photos_icon {
+            get {
+                object obj = ResourceManager.GetObject("809486_albums_media_multimedia_photo album_photos_icon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _809486_albums_media_multimedia_photo_album_photos_icon__1_ {
+            get {
+                object obj = ResourceManager.GetObject("809486_albums_media_multimedia_photo album_photos_icon (1)", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _809486_albums_media_multimedia_photo_album_photos_icon__2_ {
+            get {
+                object obj = ResourceManager.GetObject("809486_albums_media_multimedia_photo album_photos_icon (2)", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _809486_albums_media_multimedia_photo_album_photos_icon__3_ {
+            get {
+                object obj = ResourceManager.GetObject("809486_albums_media_multimedia_photo album_photos_icon (3)", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap download_png {
+            get {
+                object obj = ResourceManager.GetObject("download.png", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap download_png___Copy {
+            get {
+                object obj = ResourceManager.GetObject("download.png - Copy", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap download1 {
+            get {
+                object obj = ResourceManager.GetObject("download1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
