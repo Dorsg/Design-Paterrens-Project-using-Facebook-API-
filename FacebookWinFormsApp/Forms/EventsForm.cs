0f172cc1d86e8cf0cf0de87783e659992911eca1
tomer.b <tomer.bod@gmail.com>
2021-08-02 14:15:23 +0300

    add feats

--- FacebookWinFormsApp/Forms/EventsForm.cs
+++ FacebookWinFormsApp/Forms/EventsForm.cs
@@ -5,16 +5,46 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using FacebookWrapper.ObjectModel;
+using FacebookWrapper;
+using System.Runtime.InteropServices;
 
 namespace BasicFacebookFeatures.Forms
 {
      public partial class EventsForm : Form
      {
-          public EventsForm()
-          {
-               InitializeComponent();
-          }
-     }
+        User m_LoggedInUser;
+        public EventsForm(User i_LoggedInUser)
+        {
+            m_LoggedInUser = i_LoggedInUser;
+            InitializeComponent();
+            fetchEvents();
+        }
+
+        private void fetchEvents()
+        {
+            listBoxEvents.Items.Clear();
+            listBoxEvents.DisplayMember = "Name";
+            foreach (Event fbEvent in m_LoggedInUser.Events)
+            {
+                listBoxEvents.Items.Add(fbEvent);
+            }
+
+            if (listBoxEvents.Items.Count == 0)
+            {
+                m_LabelEventPic.Text = "no data :(";
+            }
+        }
+
+        private void listBoxEvents_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBoxEvents.SelectedItems.Count == 1)
+            {
+                Event selectedEvent = listBoxEvents.SelectedItem as Event;
+                m_EventBox.LoadAsync(selectedEvent.Cover.SourceURL);
+            }
+        }
+
+    }
 }
