e3d3d6e033a2f8c49dfd1dd53d1ebd54e5f8dec9
tomer.b <tomer.bod@gmail.com>
2021-08-22 14:49:13 +0300

    birthdayscore

---
+++ FacebookWinFormsApp/Proxy.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Proxy
+{
+    public class XMLProxy
+    {
+        private readonly XmlSerializer r_XMLSerializer;
+
+        public XMLProxy(Type i_ObjectType)
+        {
+            r_XMLSerializer = new XmlSerializer(i_ObjectType);
+        }
+
+        public void Serialize(Stream i_Stream, object i_Object)
+        {
+            try
+            {
+                r_XMLSerializer.Serialize(i_Stream, i_Object);
+            }
+            catch
+            {
+                i_Stream.Dispose();
+            }
+        }
+
+        public object Deserialize(Stream i_Stream)
+        {
+            object deserializedData = null;
+
+            try
+            {
+                deserializedData = r_XMLSerializer.Deserialize(i_Stream);
+            }
+            catch
+            {
+                i_Stream.Dispose();
+            }
+
+            return deserializedData;
+        }
+    }
+}
